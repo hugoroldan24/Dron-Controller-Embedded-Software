1de7aca7f1ea2956b0001c8f8b74f9a9fa3ac9ca
Hugo <hugoroldan060@gmail.com>
2025-09-24 16:30:57 +0200

    Update MS5611.h

--- flight controller/Core/Inc/MS5611.h
+++ flight controller/Core/Inc/MS5611.h
@@ -1,10 +1,3 @@
-/*
- * MS5611.h
- *
- *  Created on: Jul 24, 2025
- *      Author: 34684
- */
-
 #ifndef MS5611_H_
 #define MS5611_H_
 
@@ -14,3 +7,4 @@ void    init_ms5611(void);
 int32_t read_preassure(void);
 
 #endif /* MS5611_H_ */
+
